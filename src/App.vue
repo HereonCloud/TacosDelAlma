12b196d55f80907149b06354c5c737e60766b01a
Cleo <cprubino25@gmail.com>
2024-01-31 12:49:03 +0800

    Create Gallery Section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeroSection from './components/HeroSection.vue';
 import IntroSection from './components/IntroSection.vue'
+import GallerySection from './components/GallerySection.vue'
 </script>
 
 <template>
   <HeroSection></HeroSection>
   <IntroSection></IntroSection>
+  <GallerySection></GallerySection>
 </template>
 
 <style scoped>
---
+++ src/components/GallerySection.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+        <div class="left">
+            <img src="./../assets/images/Taco.png"/>
+            <div class="bottom"><img src="./../assets/images/SayYesText.png"/></div>
+        </div>
+        <div class="right">
+            <img src="./../assets/images/TacoWithLogo.png"/>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+    .container {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+    }
+    .left, .right {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 50%;
+    }
+
+    .left img {
+        width: 100%;
+    }
+
+    .left .bottom {
+        width: 100%;
+        background-color: #FECE46;
+        height: auto;
+        padding: 130px 120px;
+    }
+
+    .left .bottom img {
+        object-fit: cover;
+    }
+
+    .right{
+        background-color:#E8572B;
+        padding: 165px 105px;
+    }
+
+    .right img{
+        width: auto;
+        max-width: 100%;
+        object-fit: cover;
+    }
+
+    @media (max-width:768px) {
+        .container{ 
+            flex-direction: column-reverse;
+        }
+
+        .right, .left {
+            width: 100%;
+        }
+        .left .bottom {
+            padding: 30px 20px;
+        }
+        .right{
+            padding: 30px 20px;
+        }
+    }
+</style>
